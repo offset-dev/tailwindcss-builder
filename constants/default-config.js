4deb6f5b5de8a95cf7c3b3f4325983485dd7a4a8
Luis Rodriguez <luisrodriguez.7@icloud.com>
2021-10-06 12:44:34 -0500

    updated default config

--- constants/default-config.js
+++ constants/default-config.js
@@ -2,10 +2,12 @@ export default "module.exports = {\n" +
 	"  purge: [],\n" +
 	"  darkMode: false, // or 'media' or 'class'\n" +
 	"  theme: {\n" +
+	"    screens: {},\n" +
 	"    extend: {},\n" +
 	"  },\n" +
 	"  variants: {\n" +
 	"    extend: {},\n" +
 	"  },\n" +
 	"  plugins: [],\n" +
+	"  corePlugins: ['margin'],\n" +
 	"}";
